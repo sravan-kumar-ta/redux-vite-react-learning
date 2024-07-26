92070db81643d202905c56c46f9a4b54ee02ad7f
sravan-kumar-ta <sravan0095@gmail.com>
2024-07-26 21:17:17 +0530

    UI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React + Redux</h1>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Counter() {
+
+  return (
+    <div className="card">
+      <button>Increment</button>
+      <button>Decrement</button>
+      <p>Count is:</p>
+    </div>
+  );
+}
+
+export default Counter;
