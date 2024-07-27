e5b93577b1862c14ce8a332e9e42f7a0cbd1090d
sravan-kumar-ta <sravan0095@gmail.com>
2024-07-28 01:37:56 +0530

    immer.js

--- src/redux/counter/counterReducer.js
+++ src/redux/counter/counterReducer.js
@@ -1,3 +1,4 @@
+import { produce } from "immer";
 import { DECREMENT, INCREMENT, INCREMENTBY5 } from "./counterTypes";
 
 const initialState = {
@@ -8,22 +9,31 @@ const initialState = {
 const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-      return {
-        ...state,
-        count: state.count + 1,
-      };
+      // return {
+      //   ...state,
+      //   count: state.count + 1,
+      // };
+
+      // ---- using immer library ----
+      return produce(state, (draft) => {
+        // produce function have 2 argument (state and a call back function (draft))
+        // draft is a copy of state. so we can directly change state without copying use ...spread operator
+        draft.count = state.count + 1;
+      });
 
     case DECREMENT:
-      return {
-        ...state,
-        count: state.count - 1,
-      };
+      // return {
+      //   ...state,
+      //   count: state.count - 1,
+      // };
+      return produce(state, (draft) => {
+        draft.count = state.count - 1;
+      });
 
     case INCREMENTBY5:
-      return {
-        ...state,
-        count2: state.count2 + action.payload,
-      };
+      return produce(state, (draft) => {
+        draft.count2 = state.count2 + action.payload;
+      });
 
     default:
       return state;
